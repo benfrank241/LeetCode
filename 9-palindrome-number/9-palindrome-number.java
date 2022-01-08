086d372edbbdb7400de5416e0e9c603f39154d37
benfrank241 <62250174+benfrank241@users.noreply.github.com>
2022-01-08 16:33:47 -0600

    Time: 8 ms (53.40%), Space: 38.1 MB (93.14%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s=String.valueOf(x);
+        for(int i = 0; i < s.length()/2; i++){  
+            if(s.charAt(i) != s.charAt(s.length()-1-i)){
+                System.out.println(s.charAt(i));
+                s.charAt(s.length()-1-i);
+                return false;
+            }
+        }
+        return true;
+    }
+}
